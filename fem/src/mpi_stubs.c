c10e459431e6658a1b26d5e69b1d7f3c264b4f6c
vierinen <vierinen@04224531-1f84-46d8-96ce-875b9e37dce3>
2005-05-11 13:02:14 +0000

    removed redundant? resulttoresult
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@119 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/src/mpi_stubs.c
+++ fem/src/mpi_stubs.c
@@ -8,7 +8,7 @@
  * this will most likely be screwed before it is tested on different platforms
  */
 #include "../config.h"
-#ifndef HAVE_MPI
+/* #ifndef HAVE_MPI */
 
 void FC_FUNC_(mpi_init,MPI_INIT) 
      (int *p) { *p = 0; }
@@ -35,4 +35,4 @@ void FC_FUNC(pdneupd,PDNEUPD) () {}
 void FC_FUNC(pdseupd,PDSEUPD) () {}
 void FC_FUNC(pdsaupd,PDSAUPD) () {}
 void FC_FUNC(pdnaupd,PDNAUPD) () {}
-#endif
+/* #endif */
