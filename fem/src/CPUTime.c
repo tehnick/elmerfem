ef9e1926a93f7fa360f79171a1d934ec6ca7a972
vierinen <vierinen@04224531-1f84-46d8-96ce-875b9e37dce3>
2005-05-20 08:49:25 +0000

    sad
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@243 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/src/CPUTime.c
+++ fem/src/CPUTime.c
@@ -50,27 +50,6 @@ static struct rusage usage;
 static struct timeval tp;
 static struct timezone tzp;
 
-#ifndef HAVE_F_ETIME
-float FC_FUNC(etime,ETIME)(tt)
-float tt[2];
-{
-   int who;
-   struct rusage used;
-   who = 0;
-   getrusage(who,&used);
-   tt[0] = used.ru_utime.tv_sec+((used.ru_utime.tv_usec)/1000000.);
-   tt[1] = used.ru_stime.tv_sec+((used.ru_stime.tv_usec)/1000000.);
-   return(tt[0]+tt[1]);
-}
-#endif
-
-#ifndef HAVE_F_FLUSH
-void FC_FUNC(flush,FLUSH) (int n)
-{
-  // might as well flush a toilet...?
-}
-#endif
-
 double FC_FUNC(cputime,CPUTIME) ()
 {
   getrusage( RUSAGE_SELF, &usage );
