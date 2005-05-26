fe785ad93e2b293220d838d502b4de91aff725c7
vierinen <vierinen@04224531-1f84-46d8-96ce-875b9e37dce3>
2005-05-26 08:40:02 +0000

    compile.sh
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@339 04224531-1f84-46d8-96ce-875b9e37dce3

--- buildtools/compile.sh
+++ buildtools/compile.sh
@@ -8,14 +8,18 @@ export CVSROOT="vierinen@corona.csc.fi:/home/csc/vierinen/cvsroot"
 export CVS_RSH="ssh"
 
 if test "$1" = "clean"; then
+    # remove all temp files
     rm -Rf $EPREFIX
+    # remove all modules
+    rm -Rf $modules
 fi
 
 if test "$NPROCS" = ""; then
     NPROCS=1
 fi
 
-tmpdir=`mktemp -d build.XXXXXX`
+tmpdir=build.`hostname``date '+%Y%M%S'`
+mkdir -p $tmpdir
 tmpname=`hostname``date '+%Y%M%S'`
 
 TESTPREFIX=$EPREFIX/$tmpname
@@ -34,7 +38,8 @@ for m in $modules; do
 	make check
     fi
     cd ..
+    rm -Rf $m
 done
 
 cd $topdir
-rm -Rf $tmpdir
+# rm -Rf $tmpdir
