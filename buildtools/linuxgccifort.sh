349289b73515a2cd5feff8c7dd6f4e47c664b4a9
vierinen <vierinen@04224531-1f84-46d8-96ce-875b9e37dce3>
2005-05-26 07:46:51 +0000

    added two new targets
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@331 04224531-1f84-46d8-96ce-875b9e37dce3

---
+++ buildtools/linuxgccifort.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+
+export CC=gcc
+export CXX=g++
+export F77=ifort
+export FC=ifort
+
+sh compile.sh
---
+++ buildtools/linuxifort.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+
+export CC=icc
+export CXX=icc
+export F77=ifort
+export FC=ifort
+
+sh compile.sh
