c0c022d3557e60db44b98a6791e08b54219cfcdc
apursula <apursula@04224531-1f84-46d8-96ce-875b9e37dce3>
2005-06-23 10:55:08 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@657 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/tests/mhd/exmag.f90
+++ fem/tests/mhd/exmag.f90
@@ -1,31 +1,29 @@
 ! The additional terms in the induction equation
-! due to a time harmonic vertical field B_ac = B0 sin(wt) e_z
+! due to a time harmonic vertical field B_ac = B0 sin(wt) e_y
 
-! NOTE: not correct parameters
-
-FUNCTION acvx( model,n,t ) RESULT(c1)
+FUNCTION applied_B( model,n,t ) RESULT(c1)
    USE types
 
    TYPE(model_t) :: model
    INTEGER :: n
    REAL(KIND=dp) :: t,c1
-   REAL(KIND=dp), PARAMETER :: B0=0.0011_dp, f=35._dp, t0=1.75_dp
+   REAL(KIND=dp), PARAMETER :: B0=0.0001_dp, f=3_dp
 
 ! B_ac for nabla x ( v x B )
-   c1 = B0 * SIN(2.0_dp*PI*f*(t-t0))
+   c1 = B0 * SIN(2.0_dp*PI*f*t)
 
- END FUNCTION acvx
+ END FUNCTION applied_B
 
-FUNCTION acvt( model,n,t ) RESULT(c2)
+FUNCTION applied_dBdt( model,n,t ) RESULT(c2)
    USE types
 
    TYPE(model_t) :: model
    INTEGER :: n
    REAL(KIND=dp) :: t,c2,w
-   REAL(KIND=dp), PARAMETER :: B0=0.0011_dp, f=35._dp, t0=1.75_dp
+   REAL(KIND=dp), PARAMETER :: B0=0.0001_dp, f=3_dp
 
    w = 2.0_dp * PI * f
 ! -@B_ac/@t
-   c2 = - B0 * w * COS(w*(t-t0))
+   c2 = - B0 * w * COS(w*t)
 
- END FUNCTION acvt
+ END FUNCTION applied_dBdt
