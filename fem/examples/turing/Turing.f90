3de753336ab62dfab69f2861c2db1e9dbd1e814b
jpr <jpr@04224531-1f84-46d8-96ce-875b9e37dce3>
2005-07-07 09:09:16 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@698 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/examples/turing/Turing.f90
+++ fem/examples/turing/Turing.f90
@@ -112,7 +112,7 @@ CONTAINS
     INTEGER :: n, nd
     TYPE(Element_t), POINTER :: Element
 !------------------------------------------------------------------------------
-    REAL(KIND=dp) :: Basis(nd),dBasisdx(nd,3),ddBasisddx(nd,3,3),DetJ,LoadAtIP
+    REAL(KIND=dp) :: Basis(nd),dBasisdx(nd,3),DetJ,LoadAtIP
     LOGICAL :: Stat
     INTEGER :: i,j,t,p,q,dim
     REAL(KIND=dp), POINTER :: M(:,:), A(:,:), F(:)
@@ -137,7 +137,7 @@ CONTAINS
       ! Basis function values & derivatives at the integration point:
       !--------------------------------------------------------------
       stat = ElementInfo( Element, Nodes, IP % U(t), IP % V(t), &
-       IP % W(t), detJ, Basis, dBasisdx, ddBasisddx, .FALSE. )
+            IP % W(t), detJ, Basis, dBasisdx )
 
       s = IP % s(t) * DetJ
 
