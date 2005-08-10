0bce1b9360a86f03589f73c02f3498096f4e035a
vierinen <vierinen@04224531-1f84-46d8-96ce-875b9e37dce3>
2005-08-10 14:16:59 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@708 04224531-1f84-46d8-96ce-875b9e37dce3

--- matc/src/main.c
+++ matc/src/main.c
@@ -1,8 +1,11 @@
 
 /*
- * $Id: main.c,v 1.5 2005/05/26 12:34:54 vierinen Exp $ 
+ * $Id: main.c,v 1.6 2005/06/07 10:55:15 jpr Exp $ 
  *
  * $Log: main.c,v $
+ * Revision 1.6  2005/06/07 10:55:15  jpr
+ * *** empty log message ***
+ *
  * Revision 1.5  2005/05/26 12:34:54  vierinen
  * windows stuff
  *
@@ -42,6 +45,27 @@
 # endif
 #endif 
 
+#if !defined( __WINDOWS__ ) && ( defined( _Windows ) || defined( _WINDOWS ) )
+  #define __WINDOWS__
+#endif /* !__WINDOWS__ && ( _Windows || _WINDOWS ) */
+#if !defined( __WIN32__ ) && ( defined( WIN32 ) || defined( _WIN32 ) )
+  #ifndef __WINDOWS__
+    #define __WINDOWS__
+  #endif /* __WINDOWS__ */
+  #define __WIN32__
+#endif /* !__WIN32__ && ( WIN32 || _WIN32 ) */
+#if defined( __WINDOWS__ ) && !defined( __WIN32__ )
+  #define __WIN16__
+#endif /* __WINDOWS__ && !__WIN32__ */
+ 
+/* and now I map it to my version of the above define --tjh */
+#ifdef __WINDOWS__
+#ifndef WINDOWS
+#define WINDOWS
+#endif
+#endif /* __WINDOWS__ */
+
+
 void main( int argc, char **argv )
 {
   char strt[2000];
@@ -64,8 +88,13 @@ void main( int argc, char **argv )
       fgets( strt,  2000 , stdin);
       str = strt;      
 #endif
-      /* kludge to enable exit. */
-      if( strncasecmp(str,"exit",4) == 0  || strncasecmp(str,"quit",4) == 0 )
+      
+/* kludge to enable exit. */
+#ifdef WINDOWS
+      if( stricmp(str,"exit") == 0  || stricmp(str,"quit") == 0 )
+#else
+      if( strcasecmp(str,"exit") == 0  || strcasecmp(str,"quit") == 0 )
+#endif
       {
 	return;
       }
