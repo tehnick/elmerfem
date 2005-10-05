d34e6e42251bc259f4e68b0097a6e592bf160ec1
vierinen <vierinen@04224531-1f84-46d8-96ce-875b9e37dce3>
2005-10-05 09:56:43 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@827 04224531-1f84-46d8-96ce-875b9e37dce3

--- buildtools/linuxpathscale64.sh
+++ buildtools/linuxpathscale64.sh
@@ -4,6 +4,6 @@ export CC=pathcc
 export CXX=pathCC
 export F77=pathf90
 export FC=pathf90
-export ELMER_MODULES="umfpack matc post mathlibs eio hutiter fem"
+export ELMER_MODULES="eio elmergrid meshgen2d matc mathlibs umfpack hutiter fem post"
 
 sh compile.sh
