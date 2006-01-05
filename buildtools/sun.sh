3127fb78fea3f7ba533d310a5e1a916b7f1ff628
vierinen <vierinen@04224531-1f84-46d8-96ce-875b9e37dce3>
2006-01-05 15:34:37 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@948 04224531-1f84-46d8-96ce-875b9e37dce3

--- buildtools/sun.sh
+++ buildtools/sun.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-CC="cc"; export CC
-CXX="CC"; export CXX
-F77="f77"; export F77
-F90="f90"; export F90
+export CC=cc
+export CXX=CC
+export F77=f77
+export F90=f90
 export NPROCS=42
-
 
 bash compile.sh
