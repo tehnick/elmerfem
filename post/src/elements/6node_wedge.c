f8ba5ebd13e3d4d3920ed038c204cb8f515acf8b
jpr <jpr@04224531-1f84-46d8-96ce-875b9e37dce3>
2006-02-03 11:03:57 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@1017 04224531-1f84-46d8-96ce-875b9e37dce3

--- post/src/elements/6node_wedge.c
+++ post/src/elements/6node_wedge.c
@@ -295,6 +295,205 @@ static double elm_6node_wedge_dndw_fvalue(double *F,double u,double v,double w)
      return R;
 }
 
+
+
+/*******************************************************************************
+ *
+ *     Name:        elm_6node_wedge_point_inside(
+ *                         double *nx,double *ny,double *nz,
+ *                         double px, double py, double pz,
+ *                         double *u,double *v,double *w )
+ *
+ *     Purpose:     Find if point (px,py,pz) is inside the element, and return
+ *                  element coordinates of the point.
+ *
+ *     Parameters:
+ *
+ *         Input:   (double *) nx,ny,nz node coordinates
+ *                  (double) px,py,pz point to consider
+ *
+ *         Output:  (double *) u,v,w point in element coordinates if inside
+ *
+ *   Return value:  in/out status
+ *
+ * NOTES: the goal here can be hard for more involved element types. kind of
+ *        trivial for this one...
+ *
+ ******************************************************************************/
+int elm_6node_wedge_point_inside
+   (
+                    double *nx, double *ny, double *nz,
+        double px, double py, double pz, double *u,double *v,double *w
+   )
+{
+    double x[4],y[4],z[4],r,s,t,maxx,minx,maxy,miny,maxz,minz;
+    int i,j;
+
+    static int map[3][4] =
+    { 
+        { 4,3,2,0 }, { 4,2,1,0 }, { 4,5,3,2 } 
+    };
+
+    maxx = minx = nx[0];
+    maxy = miny = ny[0];
+    maxz = minz = nz[0];
+
+    for( i=1; i<6; i++ )
+    {
+        maxx = MAX( nx[i],maxx );
+        maxy = MAX( ny[i],maxy );
+        maxz = MAX( nz[i],maxz );
+
+        minx = MIN( nx[i],minx );
+        miny = MIN( ny[i],miny );
+        minz = MIN( nz[i],minz );
+    }
+
+    if ( px > maxx || px < minx ) return FALSE;
+    if ( py > maxy || py < miny ) return FALSE;
+    if ( pz > maxz || pz < minz ) return FALSE;
+
+    for( i=0; i<3; i++ )
+    {
+        for( j=0; j<4; j++ )
+        {
+            x[j] = nx[map[i][j]];
+            y[j] = ny[map[i][j]];
+            z[j] = nz[map[i][j]];
+        }
+
+        if ( elm_4node_tetra_point_inside( x,y,z,px,py,pz,&r,&s,&t ) )
+        {
+            *u = NodeU[map[i][0]]*r + NodeU[map[i][1]]*s + NodeU[map[i][2]]*t;
+            *v = NodeV[map[i][0]]*r + NodeV[map[i][1]]*s + NodeV[map[i][2]]*t;
+            *w = NodeW[map[i][0]]*r + NodeW[map[i][1]]*s + NodeW[map[i][2]]*t;
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+/*******************************************************************************
+ *
+ *     Name:        elm_6node_wedge_isoline
+ *
+ *     Purpose:     Extract a iso line from triangle with given threshold 
+ *
+ *     Parameters: 
+ *
+ *         Input:   (double) K, contour threshold
+ *                  (double *) F, contour quantity
+ *                  (double *) C, color quantity
+ *                  (double *) X,Y,Z vertex coords.
+ *
+ *         Output:  (line_t *)  place to store the line
+ *   
+ *   Return value:  number of lines generated (0,...,24)
+ *
+ ******************************************************************************/
+int elm_6node_wedge_isoline
+  (
+     double K, double *F, double *C,double *X,double *Y,double *Z,line_t *Line
+  )
+{
+    double f[4],c[4],x[4],y[4],z[4];
+
+    int i, j, k, n=0, above=0;
+
+    for( i=0; i<6; i++ ) above += F[i]>K;
+    if ( above == 0 || above == 6 ) return 0;
+
+    for( i=0; i<3; i++ )
+    {
+        for( j=0; j<4; j++ )
+        {
+            k = ElmWedgeFace[i][j];
+            f[j] = F[k];
+            c[j] = C[k];
+            x[j] = X[k];
+            y[j] = Y[k];
+            z[j] = Z[k];
+        }
+        n += elm_4node_quad_isoline( K,f,c,x,y,z,&Line[n] );
+    }
+
+    for( i=3; i<5; i++ )
+    {
+        for( j=0; j<3; j++ )
+        {
+            k = ElmWedgeFace[i][j];
+            f[j] = F[k];
+            c[j] = C[k];
+            x[j] = X[k];
+            y[j] = Y[k];
+            z[j] = Z[k];
+        }
+        n += elm_3node_triangle_isoline( K,f,c,x,y,z,&Line[n] );
+    }
+
+    return n;
+}
+
+/*******************************************************************************
+ *
+ *     Name:        elm_6node_wedge_isosurface
+ *
+ *     Purpose:     Extract isosurfaces for brick element.
+ *
+ *     Parameters:
+ *
+ *         Input:   (double )  K: contour threshold
+ *                  (double *) F: contour quantity values at nodes
+ *                  (double *) C: color quantity values at nodes
+ *                  (double *) X,Y,Z: node coordinates
+ *                  (double *) U,V,W: normal vector at nodes
+ *
+ *         Output:  (polygon_t *)Polygon: output triangles.
+ *
+ *   Return value:  How many triangles we've got (possible values are 0-48)...
+ *
+ ******************************************************************************/
+int elm_6node_wedge_isosurface
+   (
+       double  K,double *F,double *C, double *X,double *Y,double *Z,
+            double *U,double *V,double *W, polygon_t *Polygon
+   )
+{
+    double tx[4],ty[4],tz[4],tu[4],tv[4],tw[4],tf[4],tc[4];
+    int i,j,n;
+
+    int above = 0, below = 0;
+
+     static int map[3][4] =
+     { 
+         { 4,3,2,0 }, { 4,2,1,0 }, { 4,5,3,2 } 
+     };
+
+    for( i=0; i<6; i++ ) above += F[i]>K;
+    for( i=0; i<6; i++ ) below += F[i]<K;
+    if ( below == 6 || above == 6 ) return 0;
+
+    n = 0;
+    for( i=0; i<3; i++ )
+    {
+        for( j=0; j<4; j++ )
+        {
+            tx[j] = X[map[i][j]];
+            ty[j] = Y[map[i][j]];
+            tz[j] = Z[map[i][j]];
+            tu[j] = U[map[i][j]];
+            tv[j] = V[map[i][j]];
+            tw[j] = W[map[i][j]];
+            tf[j] = F[map[i][j]];
+            tc[j] = C[map[i][j]];
+        }
+        n += elm_4node_tetra_isosurface( K,tf,tc,tx,ty,tz,tu,tv,tw,&Polygon[n] );
+    }
+    return n;
+}
+
+
 /******************************************************************************
  *
  *     Name:        elm_6node_wedge_initialize
@@ -334,7 +533,9 @@ int elm_6node_wedge_initialize()
 
      ElementDef.FunctionValue = (double (*)())elm_6node_wedge_fvalue;
      ElementDef.Triangulate   = (int (*)())elm_6node_wedge_triangulate;
-     ElementDef.PointInside   = (int (*)())NULL;
+     ElementDef.PointInside   = (int (*)())elm_6node_wedge_point_inside;
+     ElementDef.IsoLine       = (int (*)())elm_6node_wedge_isoline;
+     ElementDef.IsoSurface    = (int (*)())elm_6node_wedge_isosurface;
 
-     return elm_add_element_type( &ElementDef );
+     return elm_add_element_type( &ElementDef ) ;
 }
