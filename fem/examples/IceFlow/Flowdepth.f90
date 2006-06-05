e86d5b38d4409262bdcff9f518f0ac41baa394fa
seddik <seddik@04224531-1f84-46d8-96ce-875b9e37dce3>
2006-06-05 11:52:25 +0000

    added computation of free surface gradient to the code. It is activated by setting the keyword "Calc Free Surface" to TRUE in the section Solver
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@1151 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/examples/IceFlow/Flowdepth.f90
+++ fem/examples/IceFlow/Flowdepth.f90
@@ -34,27 +34,25 @@ SUBROUTINE FlowdepthSolver( Model,Solver,dt,TransientSimulation )
 !------------------------------------------------------------------------------
   TYPE(Element_t),POINTER :: Element
   TYPE(ValueList_t), POINTER :: SolverParams
+  TYPE(Variable_t), POINTER :: PointerToVariable
+  TYPE(Solver_t), POINTER :: PointerToSolver
 
-  LOGICAL :: AllocationsDone = .FALSE., Found
+  LOGICAL :: AllocationsDone = .FALSE., Found, CalcFree = .FALSE.
 
-  INTEGER :: n, t, istat
-  REAL(KIND=dp) :: Norm, Gradient
+  INTEGER :: i, n, m, t, istat
+  INTEGER, POINTER :: Permutation(:), NumberOfVisits(:)
+
+  REAL(KIND=dp), POINTER :: VariableValues(:), Surface(:), GradSurface1(:),GradSurface2(:)
+  REAL(KIND=dp) :: Norm, Gradient,GradSurface(3)
 
   REAL(KIND=dp), ALLOCATABLE :: STIFF(:,:), LOAD(:), FORCE(:)
+       
 
-  SAVE STIFF, LOAD, FORCE, AllocationsDone
+  SAVE STIFF, LOAD, FORCE, Surface, GradSurface, AllocationsDone
 !------------------------------------------------------------------------------
-
-  !Allocate some permanent storage, this is done first time only:
-  !--------------------------------------------------------------
-  IF ( .NOT. AllocationsDone ) THEN
-     N = Solver % Mesh % MaxElementNodes ! just big enough for elemental arrays
-     ALLOCATE( FORCE(N), LOAD(N), STIFF(N,N), STAT=istat )
-     IF ( istat /= 0 ) THEN
-        CALL Fatal( 'FlowdepthSolve', 'Memory allocation error.' )
-     END IF
-     AllocationsDone = .TRUE.
-  END IF
+  PointerToVariable => Solver % Variable
+  Permutation  => PointerToVariable % Perm
+  VariableValues => PointerToVariable % Values
 
   ! get Gradient (change of property
   ! with respect to unit-length of
@@ -72,17 +70,59 @@ SUBROUTINE FlowdepthSolver( Model,Solver,dt,TransientSimulation )
      CALL INFO('FlowdepthSolve', Message,Level=1)
   END IF
 
+  CalcFree = GetLogical(SolverParams, 'Calc Free Surface', Found)
+  IF (.NOT. Found) THEN
+     CalcFree = .FALSE.
+  ELSE
+     CALL INFO('FlowdepthSolve', 'Free surface variable will be calculated', Level=1)
+  END IF
+
+  !Allocate some permanent storage, this is done first time only:
+  !--------------------------------------------------------------
+  IF ( (.NOT. AllocationsDone) .OR. Solver % Mesh % Changed  ) THEN
+     N = Solver % Mesh % MaxElementNodes ! just big enough for elemental arrays
+     M = Model % Mesh % NumberOfNodes
+     IF (AllocationsDone) DEALLOCATE(FORCE, LOAD, STIFF,&
+          Surface, GradSurface1, GradSurface2, NumberOfVisits)
+
+     ALLOCATE( FORCE(N), LOAD(N), STIFF(N,N),&
+          Surface(M), GradSurface1(M), GradSurface2(M), NumberOfVisits(M), STAT=istat )
+     IF ( istat /= 0 ) THEN
+        CALL Fatal( 'FlowdepthSolve', 'Memory allocation error.' )
+     END IF
+     
+     IF (CalcFree) THEN
+     ! Assign Variable for Residual (i.e., heat flux at boundaries)
+     !-------------------------------------------------------------
+        CALL VariableAdd( Solver % Mesh % Variables, Solver % Mesh, PointerToSolver, &
+             'FreeSurf', 1, Surface, Permutation )
+        CALL VariableAdd( Solver % Mesh % Variables, Solver % Mesh, PointerToSolver, &
+             'FreeSurfGrad1', 1, GradSurface1, Permutation )
+        CALL VariableAdd( Solver % Mesh % Variables, Solver % Mesh, PointerToSolver, &
+             'FreeSurfGrad2', 1, GradSurface2, Permutation )
+     END IF
+
+     AllocationsDone = .TRUE.
+  END IF
+
+  Surface = 1.0D00
+  GradSurface1 = 2.0D00
+  GradSurface2 = 3.0D00
+
+
   !Initialize the system and do the assembly:
   !------------------------------------------
   CALL DefaultInitialize()
 
+  ! bulk assembly
   DO t=1,Solver % NumberOfActiveElements
      Element => GetActiveElement(t)
      n = GetElementNOFNodes()
      CALL LocalMatrix(  STIFF, FORCE, Element, n)
      CALL DefaultUpdateEquations( STIFF, FORCE )
   END DO
-
+  
+  ! vN. conditions
   DO t=1,Solver % Mesh % NUmberOfBoundaryElements
      Element => GetBoundaryElement(t)
      n = GetElementNOFNodes()
@@ -91,12 +131,89 @@ SUBROUTINE FlowdepthSolver( Model,Solver,dt,TransientSimulation )
         CALL DefaultUpdateEquations( STIFF, FORCE )
      END IF
   END DO
-
   CALL DefaultFinishAssembly()
+
+  ! Dirichlet 
   CALL DefaultDirichletBCs()
+  !Solve the system
   Norm = DefaultSolve()
 
+  IF (Calcfree) THEN   
+     Surface = 0.0D00
+     GradSurface1 = 0.0D00
+     GradSurface2 = 0.0D00
+     NumberOfVisits = 0
+     DO t=1,Solver % NumberOfActiveElements
+        Element => GetActiveElement(t)
+        n = GetElementNOFNodes()
+        CALL GetSurfaceValue(Model, Surface, GradSurface1, GradSurface2,&
+             VariableValues, Permutation, NumberOfVisits, Element, n)
+     END DO
+     DO i=1,Model % Mesh % NumberOfNodes
+        GradSurface1(Permutation(i)) = GradSurface1(Permutation(i))/NumberOfVisits(i)
+        GradSurface2(Permutation(i)) = GradSurface2(Permutation(i))/NumberOfVisits(i)
+     END DO
+  END IF
 CONTAINS
+
+!------------------------------------------------------------------------------
+  SUBROUTINE GetSurfaceValue(Model, Surface, GradSurface1, GradSurface2,&
+       VariableValues, Permutation, NumberOfVisits, Element, n)
+!------------------------------------------------------------------------------
+    TYPE(Model_t) :: Model
+    REAL(KIND=dp) :: LocalSurf, GradSurface(3), Depth
+    INTEGER :: n
+    INTEGER, POINTER :: Permutation(:), NumberOfVisits(:)
+    REAL(KIND=dp), POINTER :: Surface(:), GradSurface1(:), GradSurface2(:), VariableValues(:)
+    TYPE(Element_t), POINTER :: Element
+!------------------------------------------------------------------------------
+    REAL(KIND=dp) :: Basis(n),dBasisdx(n,3),ddBasisddx(n,3,3),DetJ, z, U, V, W, SqrtElementMetric
+    LOGICAL :: Stat
+    INTEGER :: i,j,k,dim
+    LOGICAL :: FirstTime
+    TYPE(Nodes_t) :: Nodes
+    SAVE Nodes
+!------------------------------------------------------------------------------
+    CALL GetElementNodes( Nodes )
+
+    dim = CoordinateSystemDimension()
+    ! loop over all nodes in Element
+    DO i=1,N
+
+       j = Element % NodeIndexes(i) ! get number of node in element in physical space
+       NumberOfVisits(j) = NumberOfVisits(j) + 1
+
+       ! get local coordinates of the point i inside the element
+       U = Element % Type % NodeU(i)
+       V = Element % Type % NodeV(i)
+       W = Element % Type % NodeW(i)
+
+       ! get local information on test-functions and derivatives of the point i
+       stat = ElementInfo( Element,Nodes,U,V,W,SqrtElementMetric, &
+            Basis,dBasisdx,ddBasisddx,.FALSE.,.FALSE. )  
+       IF (DIM == 2) THEN 
+          z = Model % Nodes % y(j)
+       ELSE IF (DIM == 3) THEN
+          z = Model % Nodes % z(j)
+       ELSE
+          CALL FATAL('FlowdepthSolve', 'Flow depth for one-dimensional problem not defined!')
+       END IF
+
+       IF (NumberOfVisits(j) == 1) &
+            Surface(Permutation(j)) = z + VariableValues(Permutation(j))  
+!       DO k=1,n
+!          GradSurface1(Permutation(j)) = GradSurface1(Permutation(j)) + dBasisdx(k,1)*VariableValues(Permutation(Element % NodeIndexes(k)))
+!       END DO
+       GradSurface1(Permutation(j)) = GradSurface1(Permutation(j)) +&
+            SUM(dBasisdx(1:N,1)*VariableValues(Permutation(Element % NodeIndexes(1:N))))
+       IF (DIM > 2) &
+            GradSurface2(Permutation(j)) = GradSurface2(Permutation(j)) +&
+            SUM(dBasisdx(1:N,2)*VariableValues(Permutation(Element % NodeIndexes(1:N))))
+    END DO
+!------------------------------------------------------------------------------
+  END SUBROUTINE GetSurfaceValue
+!------------------------------------------------------------------------------
+
 
 !------------------------------------------------------------------------------
   SUBROUTINE LocalMatrix(  STIFF, FORCE, Element, n)
@@ -170,3 +287,5 @@ CONTAINS
 !------------------------------------------------------------------------------
 END SUBROUTINE FlowdepthSolver
 !------------------------------------------------------------------------------
+
+
