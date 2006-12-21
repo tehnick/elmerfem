f339a2188007cf78cedad8af65e4fb3fe2eaf16a
jpr <jpr@04224531-1f84-46d8-96ce-875b9e37dce3>
2006-12-21 12:40:59 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@1433 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/src/SolveHypre.c
+++ fem/src/SolveHypre.c
@@ -27,8 +27,9 @@
 #include "HYPRE.h"
 #include "HYPRE_parcsr_ls.h"
 
-void solvehypre_( int *nrows,int *rows, int *cols, double *vals, int *perm,
-      int *invperm, int *globaldofs, int *owner, double *xvec, double *rhsvec, int *dofs, int *pe )
+void solvehypre_( int *nrows,int *rows, int *cols, double *vals, 
+ int *perm, int *invperm, int *globaldofs, int *owner, double *xvec,
+   double *rhsvec, int *dofs, int *pe, int *ILUn, int *Rounds, double *TOL )
 {
    int i, j, k, *rcols;
    int myid, num_procs;
@@ -40,7 +41,7 @@ void solvehypre_( int *nrows,int *rows, int *cols, double *vals, int *perm,
    int solver_id;
    int print_solution, print_system;
 
-   double h, h2, *txvec, st, realtime_();
+   double  *txvec, st, realtime_();
 
    HYPRE_IJMatrix A;
    HYPRE_ParCSRMatrix parcsr_A;
@@ -51,19 +52,11 @@ void solvehypre_( int *nrows,int *rows, int *cols, double *vals, int *perm,
 
    HYPRE_Solver solver, precond;
 
-   /* Default problem parameters */
-   n = 33;
-   solver_id = 0;
-   print_solution  = 0;
-   print_system = 0;
-
 st  = realtime_();
    /* How many rows do I have? */
    local_size = *nrows;
 
-// ilower = *dofs*globaldofs[0]-*dofs+1;
-// iupper = *dofs*globaldofs[0];
-   ilower=10000000000;
+   ilower=1000000000;
    iupper=0;
    for( i=0; i<local_size / *dofs; i++ )
    {
@@ -148,77 +141,8 @@ st = realtime_();
    /* Choose a solver and solve the system */
 
    solver_id = 2;
-   /* AMG */
-   if (solver_id == 0)
-   {
-      int num_iterations;
-      double final_res_norm;
-
-      /* Create solver */
-      HYPRE_BoomerAMGCreate(&solver);
-
-      /* Set some parameters (See Reference Manual for more parameters) */
-      HYPRE_BoomerAMGSetPrintLevel(solver, 3);  /* print solve info + parameters */
-      HYPRE_BoomerAMGSetCoarsenType(solver, 6); /* Falgout coarsening */
-      HYPRE_BoomerAMGSetRelaxType(solver, 3);   /* G-S/Jacobi hybrid relaxation */
-      HYPRE_BoomerAMGSetNumSweeps(solver, 1);   /* Sweeeps on each level */
-      HYPRE_BoomerAMGSetMaxLevels(solver, 20);  /* maximum number of levels */
-      HYPRE_BoomerAMGSetTol(solver, 1e-7);      /* conv. tolerance */
-
-      /* Now setup and solve! */
-      HYPRE_BoomerAMGSetup(solver, parcsr_A, par_b, par_x);
-      HYPRE_BoomerAMGSolve(solver, parcsr_A, par_b, par_x);
-
-      /* Run info - needed logging turned on */
-      HYPRE_BoomerAMGGetNumIterations(solver, &num_iterations);
-      HYPRE_BoomerAMGGetFinalRelativeResidualNorm(solver, &final_res_norm);
-      if (myid == 0)
-      {
-         printf("\n");
-         printf("Iterations = %d\n", num_iterations);
-         printf("Final Relative Residual Norm = %e\n", final_res_norm);
-         printf("\n");
-      }
-
-      /* Destroy solver */
-      HYPRE_BoomerAMGDestroy(solver);
-   }
-   /* PCG */
-   else if (solver_id == 50)
-   {
-      int num_iterations;
-      double final_res_norm;
-
-      /* Create solver */
-      HYPRE_ParCSRPCGCreate(MPI_COMM_WORLD, &solver);
-
-      /* Set some parameters (See Reference Manual for more parameters) */
-      HYPRE_PCGSetMaxIter(solver, 1000); /* max iterations */
-      HYPRE_PCGSetTol(solver, 1e-7); /* conv. tolerance */
-      HYPRE_PCGSetTwoNorm(solver, 1); /* use the two norm as the stopping criteria */
-      HYPRE_PCGSetPrintLevel(solver, 2); /* prints out the iteration info */
-      HYPRE_PCGSetLogging(solver, 1); /* needed to get run info later */
-
-      /* Now setup and solve! */
-      HYPRE_ParCSRPCGSetup(solver, parcsr_A, par_b, par_x);
-      HYPRE_ParCSRPCGSolve(solver, parcsr_A, par_b, par_x);
-
-      /* Run info - needed logging turned on */
-      HYPRE_PCGGetNumIterations(solver, &num_iterations);
-      HYPRE_PCGGetFinalRelativeResidualNorm(solver, &final_res_norm);
-      if (myid == 0)
-      {
-         printf("\n");
-         printf("Iterations = %d\n", num_iterations);
-         printf("Final Relative Residual Norm = %e\n", final_res_norm);
-         printf("\n");
-      }
-
-      /* Destroy solver */
-      HYPRE_ParCSRPCGDestroy(solver);
-   }
    /* PCG with AMG preconditioner */
-   else if (solver_id == 1)
+   if (solver_id == 1)
    {
       int num_iterations;
       double final_res_norm;
@@ -233,7 +157,6 @@ st = realtime_();
       HYPRE_PCGSetPrintLevel(solver, 2); /* print solve info */
       HYPRE_PCGSetLogging(solver, 1); /* needed to get run info later */
 
-#if 1
       /* Now set up the AMG preconditioner and specify any parameters */
       HYPRE_BoomerAMGCreate(&precond);
       HYPRE_BoomerAMGSetPrintLevel(precond, 0); /* print amg solution info */
@@ -246,17 +169,6 @@ st = realtime_();
       /* Set the PCG preconditioner */
       HYPRE_PCGSetPrecond(solver, (HYPRE_PtrToSolverFcn) HYPRE_BoomerAMGSolve,
              (HYPRE_PtrToSolverFcn) HYPRE_BoomerAMGSetup, precond);
-#else
-      HYPRE_ParCSRPilutCreate(MPI_COMM_WORLD, &precond );
-      HYPRE_ParCSRPilutSetMaxIter( precond, 1000 );
-      HYPRE_ParCSRPilutSetDropTolerance( precond, 0.0 );
-      HYPRE_ParCSRPilutSetFactorRowSize( precond, 2500 );
-
-      /* Set the PCG preconditioner */
-       HYPRE_PCGSetPrecond(solver, (HYPRE_PtrToSolverFcn) HYPRE_ParCSRPilutSolve,
-              (HYPRE_PtrToSolverFcn) HYPRE_ParCSRPilutSetup, precond);
-#endif
-
 
       /* Now setup and solve! */
       HYPRE_ParCSRPCGSetup(solver, parcsr_A, par_b, par_x);
@@ -275,11 +187,7 @@ st = realtime_();
 
       /* Destroy solver and preconditioner */
       HYPRE_ParCSRPCGDestroy(solver);
-#if 0
       HYPRE_BoomerAMGDestroy(precond);
-#else
-      HYPRE_ParCSRPilutDestroy(precond);
-#endif
    }
    /* bicg with AMG preconditioner */
    else if (solver_id == 2)
@@ -291,17 +199,18 @@ st = realtime_();
       HYPRE_ParCSRBiCGSTABCreate(MPI_COMM_WORLD, &solver);
 
       /* Set some parameters (See Reference Manual for more parameters) */
-      HYPRE_ParCSRBiCGSTABSetMaxIter(solver, 1000); /* max iterations */
-      HYPRE_ParCSRBiCGSTABSetTol(solver, 1e-7); /* conv. tolerance */
-      HYPRE_ParCSRBiCGSTABSetStopCrit(solver, 1); /* use the two norm as the stopping criteria */
-      HYPRE_ParCSRBiCGSTABSetPrintLevel(solver, 2); /* print solve info */
-      HYPRE_ParCSRBiCGSTABSetLogging(solver, 1); /* needed to get run info later */
+      HYPRE_ParCSRBiCGSTABSetMaxIter(solver, *Rounds); /* max iterations */
+      HYPRE_ParCSRBiCGSTABSetTol(solver, *TOL);       /* conv. tolerance */
+      HYPRE_ParCSRBiCGSTABSetStopCrit(solver, 1);     /* use the two norm as the stopping criteria */
+      HYPRE_ParCSRBiCGSTABSetPrintLevel(solver, 2);   /* print solve info */
+      HYPRE_ParCSRBiCGSTABSetLogging(solver, 1);      /* needed to get run info later */
 
       HYPRE_EuclidCreate( MPI_COMM_WORLD, &precond );
       {
-          char *argv[5];
+          static char *argv[5], str[3];
           argv[0] = "-level";
-          argv[1] = "3";
+          sprintf( str, "%d", *ILUn );
+          argv[1] = str;
           HYPRE_EuclidSetParams( precond, 2, argv );
       }
 
@@ -313,80 +222,9 @@ st = realtime_();
       HYPRE_ParCSRBiCGSTABSetup(solver, parcsr_A, par_b, par_x);
       HYPRE_ParCSRBiCGSTABSolve(solver, parcsr_A, par_b, par_x);
 
-      /* Run info - needed logging turned on */
-/*
-      HYPRE_BiCGSTABGetNumIterations(solver, &num_iterations);
-      HYPRE_BiCGSTABGetFinalRelativeResidualNorm(solver, &final_res_norm);
-      if (*pe == 0)
-      {
-         printf("\n");
-         printf("Iterations = %d\n", num_iterations);
-         printf("Final Relative Residual Norm = %e\n", final_res_norm);
-         printf("\n");
-      }
-*/
-
       /* Destroy solver and preconditioner */
       HYPRE_ParCSRBiCGSTABDestroy(solver);
       HYPRE_EuclidDestroy(precond);
-   }
-   /* PCG with Parasails Preconditioner */
-   else if (solver_id == 8)
-   {
-      int    num_iterations;
-      double final_res_norm;
-
-      int      sai_max_levels = 1;
-      double   sai_threshold = 0.1;
-      double   sai_filter = 0.05;
-      int      sai_sym = 1;
-
-      /* Create solver */
-      HYPRE_ParCSRPCGCreate(MPI_COMM_WORLD, &solver);
-
-      /* Set some parameters (See Reference Manual for more parameters) */
-      HYPRE_PCGSetMaxIter(solver, 1000); /* max iterations */
-      HYPRE_PCGSetTol(solver, 1e-7); /* conv. tolerance */
-      HYPRE_PCGSetTwoNorm(solver, 1); /* use the two norm as the stopping criteria */
-      HYPRE_PCGSetPrintLevel(solver, 2); /* print solve info */
-      HYPRE_PCGSetLogging(solver, 1); /* needed to get run info later */
-
-      /* Now set up the ParaSails preconditioner and specify any parameters */
-      HYPRE_ParaSailsCreate(MPI_COMM_WORLD, &precond);
-
-      /* Set some parameters (See Reference Manual for more parameters) */
-      HYPRE_ParaSailsSetParams(precond, sai_threshold, sai_max_levels);
-      HYPRE_ParaSailsSetFilter(precond, sai_filter);
-      HYPRE_ParaSailsSetSym(precond, sai_sym);
-      HYPRE_ParaSailsSetLogging(precond, 3);
-
-      /* Set the PCG preconditioner */
-      HYPRE_PCGSetPrecond(solver, (HYPRE_PtrToSolverFcn) HYPRE_ParaSailsSolve,
-                          (HYPRE_PtrToSolverFcn) HYPRE_ParaSailsSetup, precond);
-
-      /* Now setup and solve! */
-      HYPRE_ParCSRPCGSetup(solver, parcsr_A, par_b, par_x);
-      HYPRE_ParCSRPCGSolve(solver, parcsr_A, par_b, par_x);
-
-
-      /* Run info - needed logging turned on */
-      HYPRE_PCGGetNumIterations(solver, &num_iterations);
-      HYPRE_PCGGetFinalRelativeResidualNorm(solver, &final_res_norm);
-      if (myid == 0)
-      {
-         printf("\n");
-         printf("Iterations = %d\n", num_iterations);
-         printf("Final Relative Residual Norm = %e\n", final_res_norm);
-         printf("\n");
-      }
-
-      /* Destory solver and preconditioner */
-      HYPRE_ParCSRPCGDestroy(solver);
-      HYPRE_ParaSailsDestroy(precond);
-   }
-   else
-   {
-      if (myid ==0) printf("Invalid solver id specified.\n");
    }
 
    for( k=0,i=0; i<local_size/ *dofs; i++ )
