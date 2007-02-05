4e3a56325054fe804a36ea953e759e925c50d1e5
jpr <jpr@04224531-1f84-46d8-96ce-875b9e37dce3>
2007-02-05 09:44:48 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@1537 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/config.h
+++ fem/config.h
@@ -49,7 +49,7 @@
 #define F77_FUNC_(name,NAME) name ## _
 
 /* Char pointer mangling */
-#define FC_CHAR_PTR(P,L) char *P, int L
+#define FC_CHAR_PTR(P,L) char *P
 
 /* Define to dummy `main' function (if any) required to link to the Fortran
    libraries. */
