202e99fbe95aac06ff763f8df9995403324f67e1
raback <raback@04224531-1f84-46d8-96ce-875b9e37dce3>
2007-04-27 17:15:07 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@1725 04224531-1f84-46d8-96ce-875b9e37dce3

--- elmergrid/src/femknot.c
+++ elmergrid/src/femknot.c
@@ -6277,10 +6277,9 @@ void CreateKnotsExtruded(struct FemType *dataxy,struct BoundaryType *boundxy,
     }
   }
 
+  ReorderElements(data,bound,FALSE,corder,info);    
 
   if(grid->rotate) {
-    ReorderElements(data,bound,FALSE,corder,info);    
-
     CylindricalCoordinateImprove(data,grid->rotateimprove,
 				 grid->rotateradius1,grid->rotateradius2);
 
