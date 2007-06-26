b58f02906b4fbcd0ae1c58664c63063178c571e4
jpr <jpr@04224531-1f84-46d8-96ce-875b9e37dce3>
2007-06-26 10:45:12 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@1821 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/config.h
+++ fem/config.h
@@ -32,7 +32,7 @@
 #define ELMER_LINKTYP 1
 
 /* Elmer solver default install directory */
-#define ELMER_SOLVER_HOME "/tmp/jpr/share/elmersolver"
+#define ELMER_SOLVER_HOME "/tmp/jpr/gcc/share/elmersolver"
 
 /* Define if using dynamic linking */
 #define ENABLE_DYNAMIC_LINKING 1
