8316eca6958e78c856f2f6489f06b571e40c4639
raback <raback@04224531-1f84-46d8-96ce-875b9e37dce3>
2007-06-27 12:16:58 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@1827 04224531-1f84-46d8-96ce-875b9e37dce3

--- elmergrid/src/easymesh.h
+++
@@ -1,6 +0,0 @@
-int EasymeshSave();
-#if ELMER
-int EasymeshCopy(struct FemType *data,struct BoundaryType *bound);
-#endif
-int Easymesh(int argc, char *argv[],
-	     int *nodes,int *elements,int *sides);
