81d2a160d799962b1d2767297e18c295932d0870
jpr <jpr@04224531-1f84-46d8-96ce-875b9e37dce3>
2007-09-24 10:19:39 +0000

    *** empty log message ***
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@1961 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/tests/findnorm.c
+++ fem/tests/findnorm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <ctype.h>
 
 #define MAX_LENGTH 255
@@ -8,17 +9,50 @@ int main(int argc, char **argv)
    FILE *fp = fopen( argv[1], "r" );
    char line[MAX_LENGTH+1], *ptr;
 
-   double norm, f;
-   int n;
+   static double norm, f, target_nrm, target_eps = 1e-5;
+   int n, case_ind = 0, success;
 
-   norm = 0.0;
+   success = 0;
    while( fgets( line, MAX_LENGTH, fp ) )
    {
+      if ( strstr( line, "END TEST CASE" ) ) {
+         ptr = strstr( line, "NRM=" );         
+         if ( ptr ) sscanf( ptr,"NRM=%lf", &target_nrm );
+         ptr = strstr( line, "EPS=" );         
+         if ( ptr ) sscanf( ptr,"EPS=%lf", &target_eps );         
+         case_ind++;
+         success = compare( norm, target_nrm, target_eps );
+         if ( !success ) break;
+      }
+      if ( !strstr( line, "(NRM,RELC)" ) ) continue;
       ptr = line;
-      while( *ptr != '\0' && *ptr != '.' && !isdigit(*ptr) ) ptr++;
+      while( *ptr != '\0' && *ptr!='+' && *ptr != '-' && *ptr != '.' && !isdigit(*ptr) ) ptr++;
       n = sscanf( ptr, "%lf", &f );
-      if ( n==1 && f != 0.0 )  norm = f;
+      if ( n==1 && f != 0.0 )  {
+         norm = f;
+      }
    }
 
-   fprintf( stdout, "%.12g\n", norm );
+   fprintf( stdout, "%d\n", success );
+}
+
+
+
+int compare( double norm1, double norm2, double eps )
+{
+   int n;
+
+   if ( norm1 != -1 ) {
+      if ( eps < 0 ) { 
+         if ( norm2 < norm1 )
+            return 1;
+         else
+            return 0;
+      } else  if ( 2 * fabs(norm1-norm2) / (norm1+norm2) < eps )
+         return 1;
+      else
+         return 0;
+   } else {
+      return 1;
+   }
 }
