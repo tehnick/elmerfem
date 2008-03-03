d25a9f8a729ca369950994f46a35d8f49d146cb9
mlsf <mlsf@04224531-1f84-46d8-96ce-875b9e37dce3>
2008-03-03 17:33:53 +0000

    Restructured interface to libtet in misc/Mesh3D
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@2308 04224531-1f84-46d8-96ce-875b9e37dce3

---
+++ misc/tetgen_patches/ElmerAPI.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "tetgen.h"
+using namespace std;
+//
+// C-delegator for overloaded 'tetrahedralize':
+//
+void delegate_tetrahedralize(int bs, tetgenbehavior *b, char *switches, 
+			     tetgenio *in, tetgenio *out, tetgenio *addin,
+			     tetgenio *bgmin)
+{
+  try 
+    {
+      if(bs==0)
+	tetrahedralize(b, in, out, addin, bgmin);
+      
+      if(bs==1)
+	tetrahedralize(switches, in, out, addin, bgmin);
+    }
+  catch(...)
+    {
+      cout << "libtet exception" << endl;
+      cout.flush();
+    }
+}
+//
+// Create object of class 'tetgenio'
+//
+extern "C" 
+#ifdef WIN32
+__declspec(dllexport)
+#endif
+tetgenio* CreateObjectOfTetgenio()
+{
+  return new tetgenio();
+}
