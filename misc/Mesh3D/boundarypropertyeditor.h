4d2c943f80d4b78c692f6d5542db9c076cb83cae
mlsf <mlsf@04224531-1f84-46d8-96ce-875b9e37dce3>
2008-04-03 10:39:01 +0000

    Mesh3D: Added missing files
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@2735 04224531-1f84-46d8-96ce-875b9e37dce3

---
+++ misc/Mesh3D/boundarypropertyeditor.h
@@ -0,0 +1,29 @@
+#ifndef BOUNDARYPROPERTYEDITOR_H
+#define BOUNDARYPROPERTYEDITOR_H
+
+#include <QWidget>
+#include "ui_boundarypropertyeditor.h"
+
+#define MAX_BOUNDARIES 100
+
+class BoundaryPropertyEditor : public QDialog
+{
+  Q_OBJECT
+
+public:
+  BoundaryPropertyEditor(QWidget *parent = 0);
+  ~BoundaryPropertyEditor();
+
+  Ui::boundaryPropertyDialog ui;
+
+  bool touched;
+
+private slots:
+  void applySlot();
+  void discardSlot();
+
+private:
+
+};
+
+#endif // BOUNDARYPROPERTYEDITOR_H
