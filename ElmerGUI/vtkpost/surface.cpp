1bdae2828b53f7f8280d2335690d6fa9a01a0559
mlsf <mlsf@04224531-1f84-46d8-96ce-875b9e37dce3>
2008-10-12 10:26:39 +0000

    ElmerGUI: Added some missing files
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@3389 04224531-1f84-46d8-96ce-875b9e37dce3

---
+++ ElmerGUI/vtkpost/surface.cpp
@@ -0,0 +1,97 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Elmer, A Finite Element Software for Multiphysical Problems              *
+ *                                                                           *
+ *  Copyright 1st April 1995 - , CSC - Scientific Computing Ltd., Finland    *
+ *                                                                           *
+ *  This program is free software; you can redistribute it and/or            *
+ *  modify it under the terms of the GNU General Public License              *
+ *  as published by the Free Software Foundation; either version 2           *
+ *  of the License, or (at your option) any later version.                   *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU General Public License for more details.                             *
+ *                                                                           *
+ *  You should have received a copy of the GNU General Public License        *
+ *  along with this program (in file fem/GPL-2); if not, write to the        *
+ *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,         *
+ *  Boston, MA 02110-1301, USA.                                              *
+ *                                                                           *
+ *****************************************************************************/
+
+/*****************************************************************************
+ *                                                                           *
+ *  ElmerGUI surface                                                         *
+ *                                                                           *
+ *****************************************************************************
+ *                                                                           *
+ *  Authors: Mikko Lyly, Juha Ruokolainen and Peter Råback                   *
+ *  Email:   Juha.Ruokolainen@csc.fi                                         *
+ *  Web:     http://www.csc.fi/elmer                                         *
+ *  Address: CSC - Scientific Computing Ltd.                                 *
+ *           Keilaranta 14                                                   *
+ *           02101 Espoo, Finland                                            *
+ *                                                                           *
+ *  Original Date: 15 Mar 2008                                               *
+ *                                                                           *
+ *****************************************************************************/
+
+#include <QtGui>
+#include <iostream>
+#include "epmesh.h"
+#include "vtkpost.h"
+#include "surface.h"
+
+using namespace std;
+
+Surface::Surface(QWidget *parent)
+  : QDialog(parent)
+{
+  ui.setupUi(this);
+
+  connect(ui.applyButton, SIGNAL(clicked()), this, SLOT(applyButtonClicked()));
+  connect(ui.okButton, SIGNAL(clicked()), this, SLOT(okButtonClicked()));
+  connect(ui.surfaceCombo, SIGNAL(currentIndexChanged(int)), this, SLOT(surfaceSelectionChanged(int)));
+
+  setWindowIcon(QIcon(":/icons/Mesh3D.png"));
+}
+
+Surface::~Surface()
+{
+}
+
+void Surface::applyButtonClicked()
+{
+  emit(drawSurfaceSignal());
+}
+
+void Surface::okButtonClicked()
+{
+  emit(drawSurfaceSignal());
+  close();
+}
+
+void Surface::populateWidgets(ScalarField *scalarField, int n)
+{
+  this->scalarField = scalarField;
+  this->scalarFields = n;
+
+  ui.surfaceCombo->clear();
+
+  for(int i = 0; i < n; i++) {
+    ScalarField *sf = &scalarField[i];
+    ui.surfaceCombo->addItem(sf->name);
+  }
+
+  ui.minEdit->setText(QString::number(scalarField->minVal));
+  ui.maxEdit->setText(QString::number(scalarField->maxVal));
+}
+
+void Surface::surfaceSelectionChanged(int newIndex)
+{
+  ScalarField *sf = &this->scalarField[newIndex];
+  ui.minEdit->setText(QString::number(sf->minVal));
+  ui.maxEdit->setText(QString::number(sf->maxVal));
+}
