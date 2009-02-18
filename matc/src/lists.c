b1454c545743654d71816de7351c0d9452f02591
mlsf <mlsf@04224531-1f84-46d8-96ce-875b9e37dce3>
2009-02-18 15:03:23 +0000

    matc: updated file headers
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@3987 04224531-1f84-46d8-96ce-875b9e37dce3

--- matc/src/lists.c
+++ matc/src/lists.c
@@ -1,16 +1,25 @@
-
-/******************************************************************************
+/*****************************************************************************
  *
- *       ELMER, A Computational Fluid Dynamics Program.
+ *  Elmer, A Finite Element Software for Multiphysical Problems
  *
- *       Copyright 1st April 1995 - , CSC - IT Center for Science Ltd.,
- *                                    Finland.
+ *  Copyright 1st April 1995 - , CSC - IT Center for Science Ltd., Finland
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *       All rights reserved. No part of this program may be used,
- *       reproduced or transmitted in any form or by any means
- *       without the written permission of CSC.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program (in file fem/GPL-2); if not, write to the 
+ *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ *  Boston, MA 02110-1301, USA.
  *
- ******************************************************************************/
+ *****************************************************************************/
 
 /*******************************************************************************
  *
