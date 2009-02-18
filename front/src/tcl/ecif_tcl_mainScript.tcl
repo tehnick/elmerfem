96031bbfc7437664cd7281dffa47e309aa8857fb
mlsf <mlsf@04224531-1f84-46d8-96ce-875b9e37dce3>
2009-02-18 13:50:06 +0000

    ElmerFront: Modified file headers for GPL2
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@3984 04224531-1f84-46d8-96ce-875b9e37dce3

--- front/src/tcl/ecif_tcl_mainScript.tcl
+++ front/src/tcl/ecif_tcl_mainScript.tcl
@@ -1,21 +1,25 @@
-#***********************************************************************
-#
-#       ELMER, A Computational Fluid Dynamics Program.
-#
-#       Copyright 1st April 1995 - , CSC - IT Center for Science Ltd.,
-#                                    Finland.
-#
-#       All rights reserved. No part of this program may be used,
-#       reproduced or transmitted in any form or by any means
-#       without the written permission of CSC.
-#
-#                Address: CSC - IT Center for Science Ltd.
-#                         Keilaranta 14, P.O. BOX 405
-#                         02101 Espoo, Finland
-#                         Tel.     +358 0 457 2001
-#                         Telefax: +358 0 457 2302
-#                         EMail:   Jari.Jarvinen@csc.fi
-#***********************************************************************
+#/*****************************************************************************/
+# *
+# *  Elmer, A Finite Element Software for Multiphysical Problems
+# *
+# *  Copyright 1st April 1995 - , CSC - IT Center for Science Ltd., Finland
+# * 
+# *  This program is free software; you can redistribute it and/or
+# *  modify it under the terms of the GNU General Public License
+# *  as published by the Free Software Foundation; either version 2
+# *  of the License, or (at your option) any later version.
+# * 
+# *  This program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with this program (in file fem/GPL-2); if not, write to the 
+# *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# *  Boston, MA 02110-1301, USA.
+# *
+# *****************************************************************************/
 
 #***********************************************************************
 #Program:   ELMER Front 
@@ -185,8 +189,8 @@ proc startGUI {} {
 proc setElmerFrontPaths {} {
     global Info env
 
-    set Info(ELMER_FRONT_INSTALL_LIB) "/wrk/jpr/ff//share/elmerfront/lib"
-    set Info(ELMER_FRONT_INSTALL_TCL) "/wrk/jpr/ff//share/elmerfront/tcl"
+    set Info(ELMER_FRONT_INSTALL_LIB) "/usr/local/share/elmerfront/lib"
+    set Info(ELMER_FRONT_INSTALL_TCL) "/usr/local/share/elmerfront/tcl"
 
     if { [info exists env(ELMER_HOME)] } {
       set Info(ELMER_FRONT_INSTALL_LIB) "$env(ELMER_HOME)/share/elmerfront/lib"
@@ -297,11 +301,11 @@ proc initEnvironment {} {
     set Info(ELMER_HOME) $env(ELMER_HOME)
 
     if { ![file isdirectory $Info(ELMER_HOME)] } {
-      set Info(ELMER_HOME) "/wrk/jpr/ff/"
+      set Info(ELMER_HOME) "/usr/local"
     }
 
   } else {
-    set Info(ELMER_HOME) "/wrk/jpr/ff/"
+    set Info(ELMER_HOME) "/usr/local"
   }
 
 
