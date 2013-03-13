6238630c1bcddc55167258081878a1d8b60f5b40
fgillet <fgillet@04224531-1f84-46d8-96ce-875b9e37dce3>
2013-03-13 13:54:34 +0000

    elmerice: add solvers for Robin inverse method (inversion of basal slip coefficient)
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@6090 04224531-1f84-46d8-96ce-875b9e37dce3

--- elmerice/examples/InverseMethods/RunTest.sh
+++ elmerice/examples/InverseMethods/RunTest.sh
@@ -3,11 +3,16 @@
 # Define the number of partitions (has to be a parallel run, i.e. np>1 !!)
 export np=8 
 
+# Test to run 
+
+file=Adjoint_Beta.sif  # Control inverse method
+#file=Robin_Beta.sif   # Robin inverse method
+
 ## make compilation and Mesh
 make clean
 make 
 make Mesh
 
 # Run the test
-echo Adjoint_Beta.sif > ELMERSOLVER_STARTINFO
+echo $file > ELMERSOLVER_STARTINFO
 mpirun -n $np ElmerSolver_mpi
