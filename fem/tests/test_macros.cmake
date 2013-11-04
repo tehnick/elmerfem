45e3d6e4a7100acd191ec7a1e1afbed0fc387659
ilvonen <ilvonen@04224531-1f84-46d8-96ce-875b9e37dce3>
2013-11-04 15:41:07 +0000

    Fixed few CMake issues with libm and MPI headers
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@6408 04224531-1f84-46d8-96ce-875b9e37dce3

--- fem/tests/test_macros.cmake
+++ fem/tests/test_macros.cmake
@@ -8,13 +8,14 @@ macro(ADD_ELMER_TEST test_name)
       -DELMERSOLVER_BIN=${ELMERSOLVER_BIN}
       -DFINDNORM_BIN=${FINDNORM_BIN}
       -DTEST_SOURCE=${CMAKE_CURRENT_SOURCE_DIR}
+      -DPROJECT_SOURCE_DIR=${PROJECT_SOURCE_DIR}
       -P ${CMAKE_CURRENT_SOURCE_DIR}/runtest.cmake)
 endmacro()
 
 
 macro(RUN_ELMER_TEST)
-  set(ENV{ELMER_LIB} "/home/silvonen/compile/ElmerGitSvn/code/fem/src")
-  set(ENV{ELMER_HOME} "/home/silvonen/compile/ElmerGitSvn/code/fem/src")
+  set(ENV{ELMER_LIB} "${PROJECT_SOURCE_DIR}/fem/src")
+  set(ENV{ELMER_HOME} "${PROJECT_SOURCE_DIR}/fem/src")
   execute_process(COMMAND ${ELMERSOLVER_BIN} OUTPUT_FILE "test.log"
     ERROR_FILE "test.log")
   execute_process(COMMAND ${FINDNORM_BIN} ${CMAKE_CURRENT_BINARY_DIR}/test.log
