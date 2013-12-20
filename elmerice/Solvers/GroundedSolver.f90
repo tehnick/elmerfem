7fa80aa99f33d282211fe402c0207fe1c6dec221
rupertgladstone <rupertgladstone@04224531-1f84-46d8-96ce-875b9e37dce3>
2013-12-20 01:23:42 +0000

    Tidying groundedSolver, removed unused code, corrected indentations, added option to specify name of material property for reading in bedrock.
    git-svn-id: svn://svn.code.sf.net/p/elmerfem/code/trunk@6490 04224531-1f84-46d8-96ce-875b9e37dce3

--- elmerice/Solvers/GroundedSolver.f90
+++ elmerice/Solvers/GroundedSolver.f90
@@ -63,39 +63,39 @@ SUBROUTINE GroundedSolver( Model,Solver,dt,TransientSimulation )
   USE DefUtils
 
   IMPLICIT NONE
-!------------------------------------------------------------------------------
+  !------------------------------------------------------------------------------
   TYPE(Solver_t) :: Solver
   TYPE(Model_t) :: Model
 
   REAL(KIND=dp) :: dt
   LOGICAL :: TransientSimulation
 
-!------------------------------------------------------------------------------
-! Local variables
-!------------------------------------------------------------------------------
+  !------------------------------------------------------------------------------
+  ! Local variables
+  !------------------------------------------------------------------------------
 
   TYPE(Element_t),POINTER :: Element
   TYPE(ValueList_t), POINTER :: Material, SolverParams
   TYPE(Variable_t), POINTER :: PointerToVariable, bedrockVar
   TYPE(Nodes_t), SAVE :: Nodes
 
-  LOGICAL :: AllocationsDone = .FALSE., GotIt, stat, Parallel, Quadratic
+  LOGICAL :: AllocationsDone = .FALSE., GotIt, stat
 
-  INTEGER :: i, mn, n, t, Nn, istat, DIM, MSum, ZSum
+  INTEGER :: i, mn, n, t, Nn, istat, DIM, MSum, ZSum, bedrockSource
   INTEGER, POINTER :: Permutation(:), bedrockPerm(:)
 
   REAL(KIND=dp), POINTER :: VariableValues(:)
   REAL(KIND=dp) :: z, toler
   REAL(KIND=dp), ALLOCATABLE :: zb(:)
 
-  CHARACTER(LEN=MAX_NAME_LEN) :: SolverName = 'GroundedSolver', bedrockVarName
+  CHARACTER(LEN=MAX_NAME_LEN) :: SolverName = 'GroundedSolver', bedrockName
+
+  INTEGER,PARAMETER :: MATERIAL_DEFAULT = 1, MATERIAL_NAMED = 2, VARIABLE = 3
        
-  SAVE AllocationsDone, DIM, SolverName, zb
-  SAVE toler, Parallel, Quadratic
+  SAVE AllocationsDone, DIM, SolverName, zb, toler
   !------------------------------------------------------------------------------
 
-  NULLIFY(bedrockPerm)
-  NULLIFY(bedrockVar)
+!  NULLIFY(bedrockPerm,bedrockVar)
 
   PointerToVariable => Solver % Variable
   Permutation  => PointerToVariable % Perm
@@ -104,136 +104,130 @@ SUBROUTINE GroundedSolver( Model,Solver,dt,TransientSimulation )
   CALL INFO(SolverName, 'Computing grounded mask from geometry', level=3)
 
   !--------------------------------------------------------------
-  !Allocate some permanent storage, this is done first time only:
+  ! Allocate some permanent storage:
   !--------------------------------------------------------------
-
   IF ( (.NOT. AllocationsDone) .OR. Solver % Mesh % Changed ) THEN
-    DIM = CoordinateSystemDimension()
-    mn = Solver % Mesh % MaxElementNodes
-
-    IF (AllocationsDone) DEALLOCATE(zb)
-
-    ALLOCATE(zb(mn), STAT=istat )
-          
-    IF ( istat /= 0 ) THEN
-      CALL FATAL( SolverName, 'Memory allocation error.' )
-    END IF
-
-    AllocationsDone = .TRUE.
-    CALL INFO( SolverName, 'Memory allocation done.',Level=1 )
-
-    !---------------------------
-    ! Environnement parallele ?
-    !---------------------------
-    Parallel = .FALSE.
-    IF ( ParEnv % PEs > 1 ) Parallel = .TRUE.
-
-    ! quadratic or linear elements (not used for now)
-    Quadratic = .FALSE.
-    Element => GetActiveElement(1)
-    n = GetElementNOFNodes()
-    IF ( Element % Type % ElementCode/n == 102 ) Quadratic = .TRUE.
-
+     DIM = CoordinateSystemDimension()
+     mn = Solver % Mesh % MaxElementNodes
+     IF (AllocationsDone) DEALLOCATE(zb)     
+     ALLOCATE(zb(mn), STAT=istat )
+     IF ( istat /= 0 ) THEN
+        CALL FATAL( SolverName, 'Memory allocation error.' )
+     END IF
+     CALL INFO( SolverName, 'Memory allocation done.',Level=1 )
+     AllocationsDone = .TRUE.
   END IF
-  ! End first time
+  
+  SolverParams => GetSolverParams()
+  toler = GetConstReal(SolverParams, 'Toler', GotIt)
+  IF (.NOT.GotIt) THEN
+     CALL FATAL(SolverName, 'No tolerance given for the Grounded Mask.')
+  END IF
 
-  !--------------------------------------------
-  ! First loop, where y > zb GroundedMask = -1 
-  !--------------------------------------------
+  bedrockName = GetString(SolverParams, 'Bedrock Variable', GotIt)
+  IF (GotIt) THEN
+     bedrockSource = VARIABLE
+     CALL info(SolverName, 'Bedrock Variable name found', level=8)
+  ELSE
+     bedrockName = GetString(SolverParams, 'Bedrock Material', GotIt)
+     IF (GotIt) THEN
+        bedrockSource = MATERIAL_NAMED
+        CALL info(SolverName, 'Bedrock Material name found', level=8)
+     ELSE
+        bedrockSource = MATERIAL_DEFAULT     
+        CALL info(SolverName, 'No Bedrock Variable or Material; searching for material \"Min Zs Bottom\".', level=8)
+     END IF
+  END IF
+
+
+    
+  !--------------------------------------------------------------
+  ! Grounded/floating loop based on height of base above bedrock.
+  !--------------------------------------------------------------
   DO t = 1, Solver % NumberOfActiveElements
-    Element => GetActiveElement(t)
-!     IF (ParEnv % myPe .NE. Element % partIndex) CYCLE
-    n = GetElementNOFNodes()
-
-    SolverParams => GetSolverParams()
-    toler = GetConstReal(SolverParams, 'Toler', GotIt)
-    IF (.NOT.GotIt) THEN
-      CALL FATAL(SolverName, 'No tolerance given for the Grounded Mask.')
-    END IF
-
-    bedrockVarName = GetString(SolverParams, 'Bedrock Variable', GotIt)
-    IF (GotIt) THEN
-       bedrockVar => VariableGet(Model % Mesh % Variables, bedrockVarName )
-       bedrockPerm => bedrockVar % Perm
-       zb(1:n) =  bedrockVar % values(bedrockPerm(Element % NodeIndexes)) + toler
-       NULLIFY(bedrockPerm)
-       NULLIFY(bedrockVar)
-    ELSE
-       CALL info(SolverName, 'Bedrock Variable name not found, searching for material property \"Min Zs Bottom\".')
-       Material => GetMaterial( Element )
-       zb(1:n) = ListGetReal( Material,'Min Zs Bottom',n , & 
-            Element % NodeIndexes, GotIt ) + toler
-    END IF
-
-    CALL GetElementNodes( Nodes )
-
-    DO i = 1, n
-
-      Nn = Permutation(Element % NodeIndexes(i))
-      IF (Nn==0) CYCLE
-
-      IF (DIM == 2) THEN
-        z = Nodes % y( i )
-      ELSE IF (DIM == 3) THEN
-        z = Nodes % z( i )
-      END IF
-
-      ! Geometrical condition
-      ! if the node is above the bedrock (plus the tolerance)
-      ! then its mask is -1 (floating)
-      ! otherwise it is 1 (grounded)
-
-      IF (z > zb(i)) THEN
-        VariableValues(Nn) = -1.0_dp
-      ELSE
-        VariableValues(Nn) = 1.0_dp
-      END IF
-
-    END DO
-  END DO
-
-  ! attribute zeros to the mask, corresponding to the Grounding Line
-  ! loop over each element:
-  ! if the sum of the element masks is lower than the element number of nodes minus the number of zeros, then each mask equal to 1 is modified to 0
-
-  DO t = 1, Solver % NumberOfActiveElements
-
-    Element => GetActiveElement(t)
-    !IF (ParEnv % myPe .NE. Element % partIndex) CYCLE
-    n = GetElementNOFNodes()
-
-    CALL GetElementNodes( Nodes )
-    MSum = 0
-    ZSum = 0
-
-    DO i = 1, n
-      Nn = Permutation(Element % NodeIndexes(i))
-      IF (Nn==0) CYCLE
-      MSum = MSum + VariableValues(Nn)
-      IF (VariableValues(Nn) == 0.0_dp) ZSum = ZSum + 1.0_dp
-    END DO
-
-    IF (MSum + ZSum < n) THEN
-      DO i = 1, n
+     Element => GetActiveElement(t)
+     n = GetElementNOFNodes()
+     
+     SELECT CASE(bedrockSource)
+     CASE (VARIABLE)
+        bedrockVar => VariableGet(Model % Mesh % Variables, bedrockName )
+        IF (.NOT. ASSOCIATED(bedrockVar)) CALL FATAL(SolverName,"Could not find bedrock variable")
+        bedrockPerm => bedrockVar % Perm
+        zb(1:n) =  bedrockVar % values(bedrockPerm(Element % NodeIndexes)) + toler
+        NULLIFY(bedrockPerm)
+        NULLIFY(bedrockVar)
+     CASE (MATERIAL_NAMED)
+        Material => GetMaterial( Element )
+        zb(1:n) = ListGetReal( Material,bedrockName, n , & 
+             Element % NodeIndexes, GotIt ) + toler
+        IF (.NOT. GotIt) CALL FATAL(SolverName,"Could not find bedrock material")
+     CASE (MATERIAL_DEFAULT)
+        Material => GetMaterial( Element )
+        zb(1:n) = ListGetReal( Material,'Min Zs Bottom',n , & 
+             Element % NodeIndexes, GotIt ) + toler
+        IF (.NOT. GotIt) CALL FATAL(SolverName,"Could not find bedrock material")
+     END SELECT
+     
+     CALL GetElementNodes( Nodes )
+     
+     DO i = 1, n
         Nn = Permutation(Element % NodeIndexes(i))
         IF (Nn==0) CYCLE
-
-        IF (VariableValues(Nn) == 1.0_dp) THEN
-          VariableValues(Nn) = 0.0_dp
-
-          IF (DIM==2) PRINT *, 'Grounding Line, x', Nodes % x( i )
-          IF (DIM==3) PRINT *, 'Grounding Line, (x,y)', Nodes % x( i ), Nodes % y( i )
-
+        IF (DIM == 2) THEN
+           z = Nodes % y( i )
+        ELSE IF (DIM == 3) THEN
+           z = Nodes % z( i )
         END IF
-      END DO
-    END IF
+        
+        ! Geometrical condition. Is the node is above the bedrock 
+        ! (plus the tolerance)?  Note: zb includes tolerance.
+        IF (z > zb(i)) THEN
+           VariableValues(Nn) = -1.0_dp
+        ELSE
+           VariableValues(Nn) = 1.0_dp
+        END IF
+     END DO
   END DO
-
+  
+  !--------------------------------------------------------------
+  ! Grounding line loop to label grounded points at grounding Line.
+  !--------------------------------------------------------------
+  ! Loop over each element:
+  !  if the sum of the element masks is lower than the element number 
+  !  of nodes minus the number of zeros (i.e. if the element has at 
+  !  least one floating node), then each mask equal to 1 is modified 
+  !  to 0 (i.e. this node is on the grounding line).  
+  DO t = 1, Solver % NumberOfActiveElements     
+     Element => GetActiveElement(t)
+     n = GetElementNOFNodes()
+     CALL GetElementNodes( Nodes )
+     MSum = 0
+     ZSum = 0
+     
+     DO i = 1, n
+        Nn = Permutation(Element % NodeIndexes(i))
+        IF (Nn==0) CYCLE
+        MSum = MSum + VariableValues(Nn)
+        IF (VariableValues(Nn) == 0.0_dp) ZSum = ZSum + 1.0_dp
+     END DO
+     
+     IF (MSum + ZSum < n) THEN
+        DO i = 1, n
+           Nn = Permutation(Element % NodeIndexes(i))
+           IF (Nn==0) CYCLE
+           IF (VariableValues(Nn) == 1.0_dp) THEN
+              VariableValues(Nn) = 0.0_dp
+              IF (DIM==2) PRINT *, 'Grounding Line, x', Nodes % x( i )
+              IF (DIM==3) PRINT *, 'Grounding Line, (x,y)', Nodes % x( i ), Nodes % y( i )
+           END IF
+        END DO
+     END IF
+  END DO
+  
   IF ( ParEnv % PEs>1 ) CALL ParallelSumVector( Solver % Matrix, VariableValues, 1 )
-
+ 
   CALL INFO( SolverName , 'Done')
-
-!------------------------------------------------------------------------------
+ 
 END SUBROUTINE GroundedSolver 
 !------------------------------------------------------------------------------
 
